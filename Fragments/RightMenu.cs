72dada5f3a3a2074cf9e22e87e8a255196dd9151
MikhasNew <87332000+MikhasNew@users.noreply.github.com>
2022-06-13 08:03:37 +0300

    Добавьте файлы проекта.

---
+++ Fragments/RightMenu.cs
@@ -0,0 +1,24 @@
+﻿using Android.OS;
+
+using Android.Views;
+
+using AndroidX.Fragment.App;
+
+namespace NavigationDrawerStarter.Fragments
+{
+    public class RightMenu : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.right_menu, container, false);
+        }
+    }
+}
